7c1e175bf608e75a932d80436f8107c5c03b63d3
piranhap_local <danielmperagallo@gmail.com>
2025-04-04 15:29:44 -0600

    the tool works, one column is missing

--- scrapo.py
+++ scrapo.py
@@ -1,10 +1,16 @@
+# imports and variables
+################################
 from pdfquery import PDFQuery
 import re
+import pandas as pd
 
 pdf = PDFQuery('p1.pdf')
 pdf.load()
+################################
 
+#Data Collection 
 
+################################
 gender = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 496.411, 117.541, 503.911")').text()
 m_name = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 470.911, 120.321, 478.411")').text()
 l_name = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 458.161, 130.465, 465.661")').text()
@@ -23,9 +29,9 @@ email = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 305.911, 263.139, 313.411"
 phone = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 293.161, 147.992, 300.661")').text()
 address = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 280.411, 266.242, 287.911")').text()
 ssn = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 254.911, 226.764, 262.411")').text()
-passport_n = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 242.161, 154.664, 249.661")').text()
-passport_issued_date = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 233.911, 190.923, 241.411")').text()
-passport_exp_date = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 225.661, 193.42, 233.161")').text()
+#passport_n = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 242.161, 154.664, 249.661")').text()
+#passport_issued_date = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 233.911, 190.923, 241.411")').text()
+#passport_exp_date = pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 225.661, 193.42, 233.161")').text()
 #passport_code = str(pdf.pq('LTTextBoxHorizontal:in_bbox("101.285, 217.118, 282.721, 224.618]")')).text()
 
 
@@ -135,21 +141,11 @@ alignment = align_raw
 dnd_traits = pdf.pq('LTTextBoxHorizontal:contains("Charisma")').text().split()
 charm, dnd_charm, const, dnd_const, dext, dnd_dext, inte, dnd_int, stren, dnd_str, wis, dnd_wise = dnd_traits
 
+##########################
 
+# Assigning collected data to a value to print
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+##########################
 
 scraped_data = {
     "gender": gender,
@@ -169,12 +165,14 @@ scraped_data = {
     "phone": phone,
     "address": address,
     "ssn": ssn,
-    #"passport_number": passport_n,
-    #"passport_issued_date": passport_issued_date,
-    #"passport_exp_date": passport_exp_date,
-    #"passport_code": passport_code,
     "drivers_license_number": drivers_license_n,
+    "drivers_license_state": drivers_license_state,
+    "drivers_license_issued_date": drivers_license_issued_date,
+    "drivers_license_exp_date": drivers_license_exp_date,
     "car_model": car_model,
+    "car_plate_number": car_plate_number,
+    "car_plate_state_issued": car_plate_state_issued,
+    "car_plate_date_issued": car_plate_date_issued,
     "hair_color": hair_color,
     "eyes_color": eyes_color,
     "height": height,
@@ -213,7 +211,96 @@ scraped_data = {
     "dnd_wise": dnd_wise,
 }
 
-for key, value in scraped_data.items():
-    print(f"{value}")
+#for key, value in scraped_data.items(): to print stuff to see if it grabs it
+#   print(f"{value}")
 
+# Headers for the spreadsheet
+headers = [
+    "Gender", "First Name", "Middle Name", "Last Name", "Initials", "Mother's maiden name",
+    "Birthday", "Birthplace", "Zodiacal Sign", "Username", "Password", "Password Hash (MD5)",
+    "Password Hash (SHA1)", "E-mail", "Phone", "Address", "SSN (2141123212) - issued in maryland",
+    "Drivers License Number", "Drivers License State issued", "Drivers License issued date",
+    "Drivers License exp date", "Car", "Car License Plate Number", "Car License Plate state issued",
+    "Car License Plate date issued", "Hair Color", "Eyes Color", "Height", "Weight", "Shoe Size",
+    "Blood Type", "GUID", "UniqID", "Western Union MTCN", "MoneyGram MTCN", "FICO Credit Score",
+    "Experian", "Equifax", "Vantage Score", "FICO NextGen Risk Score",
+    "FICO Small Business Scoring Service(SBSS)", "Religion", "Political side", "Favorite Color",
+    "Favorite comfort food", "Favorite Cereal", "Favorite Season", "Favorite Animal",
+    "Lucky number", "Preparer Tax Identification Number (PTIN)", "Interim PTIN (temporary PTIN)",
+    "Employer Identification Number (EIN)", "Individual Taxpayer Identification Number (ITIN)",
+    "Adoption Taxpayer Identification Number (ATIN)", "Alignment", "Charisma", "Constitution",
+    "Dexterity", "Intelligence", "Strength", "Wisdom"
+]
 
+# Map the scraped data to the headers
+data = [
+    scraped_data.get("gender", ""),
+    scraped_data.get("first_name", ""),
+    scraped_data.get("middle_name", ""),
+    scraped_data.get("last_name", ""),
+    scraped_data.get("initials", ""),
+    scraped_data.get("maiden_name", ""),
+    scraped_data.get("birthday", ""),
+    scraped_data.get("birthplace", ""),
+    scraped_data.get("zodiac_sign", ""),
+    scraped_data.get("username", ""),
+    scraped_data.get("password", ""),
+    scraped_data.get("password_hash_md5", ""),
+    scraped_data.get("password_hash_sha1", ""),
+    scraped_data.get("email", ""),
+    scraped_data.get("phone", ""),
+    scraped_data.get("address", ""),
+    scraped_data.get("ssn", ""),
+    scraped_data.get("drivers_license_number", ""),
+    scraped_data.get("drivers_license_state", ""),
+    scraped_data.get("drivers_license_issued_date", ""),
+    scraped_data.get("drivers_license_exp_date", ""),
+    scraped_data.get("car_model", ""),
+    scraped_data.get("car_plate_number", ""),
+    scraped_data.get("car_plate_state_issued", ""),
+    scraped_data.get("car_plate_date_issued", ""),
+    scraped_data.get("hair_color", ""),
+    scraped_data.get("eyes_color", ""),
+    scraped_data.get("height", ""),
+    scraped_data.get("weight", ""),
+    scraped_data.get("shoe_size", ""),
+    scraped_data.get("blood_type", ""),
+    scraped_data.get("guid", ""),
+    scraped_data.get("uniqid", ""),
+    scraped_data.get("mtcn", ""),
+    scraped_data.get("mtcn_mg", ""),
+    scraped_data.get("credit_score_fico", ""),
+    scraped_data.get("credit_score_exp", ""),
+    scraped_data.get("credit_score_equi", ""),
+    scraped_data.get("credit_score_vant", ""),
+    scraped_data.get("credit_score_nextgen", ""),
+    scraped_data.get("credit_score_sbss", ""),
+    scraped_data.get("religion", ""),
+    scraped_data.get("political_side", ""),
+    scraped_data.get("favorite_color", ""),
+    scraped_data.get("favorite_food", ""),
+    scraped_data.get("favorite_cereal", ""),
+    scraped_data.get("favorite_season", ""),
+    scraped_data.get("favorite_animal", ""),
+    scraped_data.get("lucky_number", ""),
+    scraped_data.get("ptin", ""),
+    scraped_data.get("temp_ptin", ""),
+    scraped_data.get("ein", ""),
+    scraped_data.get("itin", ""),
+    scraped_data.get("atin", ""),
+    scraped_data.get("alignment", ""),
+    scraped_data.get("dnd_charm", ""),
+    scraped_data.get("dnd_const", ""),
+    scraped_data.get("dnd_dext", ""),
+    scraped_data.get("dnd_int", ""),
+    scraped_data.get("dnd_str", ""),
+    scraped_data.get("dnd_wise", "")
+]
+
+# Create a Dataframe
+df = pd.DataFrame([data], columns=headers)
+
+# Save all to an excel file 
+output_file = "junk_people.xlsx"
+df.to_excel(output_file, index=False)
+print("Data written in excel")
